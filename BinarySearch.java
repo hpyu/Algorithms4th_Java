e058a19ec041041fa70f5007bec9713b192d95aa
Haipeng Yu <hyu35@uclive.ac.nz>
2018-07-17 11:59:23 +1200

    Add Java doc comment and parameter checking

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,13 +1,53 @@
+/**
+ * Compilation:   javac BinarySearch.java
+ * Execution:     java BinarySearch
+ * Dependencies:  In StdOut Arrays
+ * Data Files:    Algs4-data/{tinyT.txt, tinyW.txt, largeT.txt, largeW.txt}
+ *
+ * % java BinarySearch algs4-data/tinyW.txt  algs4-data/tinyT.txt
+ * not found: 50
+ * not found: 99
+ * not found: 13
+ * finished.
+ */
+
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
 
+/**
+ * The {@code BinarySearch} class provides a static method {@indexOf} to find
+ * the index of a integer in a sorted array
+ * <p>
+ * The indexOf method takes logarithmic time in operation
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ * @author Haipeng Yu
+ */
 public class BinarySearch
 {
-    public static int rank(int key, int[] a)
+    private boolean isSorted(int[] a)
     {
+        for (int i = 1; i < a.length - 1; i++)
+        {
+            if (a[i] < a[i-1])
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Find the index of key in array a
+     * @param a sorted int array
+     * @param key key to search in array
+     * @return index of key in array {@code a} if present, {@code -1} otherwise
+     */
+    public static int indexOf(int[] a, int key)
+    {
+        if (!isSorted(a)) throw new IllegalArgumentException("Array is not sorted increasingly.")
+
         int lo = 0;
         int hi = a.length - 1;
 
@@ -22,6 +62,9 @@ public class BinarySearch
         return -1;
     }
 
+    @Deprecated
+    public static int rank(int key, int[] a) { return indexOf(a, key); }
+
     public static void main(String[] args)
     {
         In inw = new In(args[0]);
@@ -33,9 +76,8 @@ public class BinarySearch
         while (!inT.isEmpty())
         {
             int key = inT.readInt();
-            int ret = rank(key, whitelist);
 
-            if (ret == -1)
+            if (indexOf(whitelist, key) == -1)
             {
                 StdOut.printf("not found: %d\n", key);
             }
@@ -43,3 +85,27 @@ public class BinarySearch
         StdOut.println("finished.");
     }
 }
+
+/******************************************************************************
+ *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
--- WhiteBoard.java
+++ WhiteBoard.java
@@ -1,3 +1,13 @@
+/*****************************************************************************
+ * Compilation:     javac WhiteBoard.java
+ * Execution:       java WhiteBoard
+ * Dependencies:    StdDraw, StdOut StdRandom Counter Math Arrays
+ * Data Files:      None
+ *
+ * Enclose any code here just for scratching and verification
+ *
+ *****************************************************************************/
+
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
@@ -7,14 +17,24 @@ import java.util.Arrays;
 
 import static java.lang.Math.log;
 
+/**
+ * Class {@code WhiteBoard} is a place where I write code pieces
+ * just for verifying code.
+ */
 public class WhiteBoard {
-    public static void drawFuncValues (int N)
+
+    // This class should no be initialized.
+    private WhiteBoard() { };
+
+    /**
+     * Draw function curves with StdDraw
+     *
+     * @param N Number of random number
+     * @throws IllegalArgumentException if {@code N} is negative
+     */
+    public static void drawFunctionValues (int N)
     {
-        if (N < 0)
-        {
-            StdOut.println("N should be positive number!");
-            return;
-        }
+        if (N < 0) throw new IllegalArgumentException("N is negative");
 
         StdDraw.setXscale(0, N);
         StdDraw.setYscale(0, N*N);
@@ -24,16 +44,17 @@ public class WhiteBoard {
             StdDraw.point(i, i*i);
             StdDraw.point(i, i*log(i));
         }
-
     }
 
-    public static void drawArraryOfRandomValues (int N)
+    /**
+     * Draw random array in column shape sort or not sort
+     *
+     * @param N Number of random number
+     * @throws IllegalArgumentException if {@code N} is negative
+     */
+    public static void drawArrayOfRandomValues (int N)
     {
-        if (N < 0)
-        {
-            StdOut.println("N should be positive number!");
-            return;
-        }
+        if (N < 0) throw new IllegalArgumentException("N is negative");
 
         double[] a = new double[N];
         for (int i = 0; i < N; i++) a[i] = StdRandom.uniform();
@@ -46,11 +67,17 @@ public class WhiteBoard {
             double rh = a[i]/2.0;
             StdDraw.filledRectangle(x, y, rw, rh);
         }
-
     }
 
+    /**
+     * Implement head/tail coin flip with Counter and StdRandom
+     *
+     * @param T The times to flip the coin
+     * @throws IllegalArgumentException if {@code T} is negative
+     */
     public static void flip (int T)
     {
+        if (T < 0) throw new IllegalArgumentException("T is negative");
         assert T > 0 : "Non-Positive time number";
         Counter heads = new Counter("head");
         Counter tails = new Counter("tail");
@@ -68,10 +95,41 @@ public class WhiteBoard {
         int d = Math.abs(heads.tally() - tails.tally());
         StdOut.printf("deta: %d, %%%.6f\n", d, (d*100.0)/T);
     }
+
+    /**
+     * Test methods
+     *
+     * @param args
+     */
     public static void main (String[] args)
     {
-        //drawFuncValues(100);
-        //drawArraryOfRandomValues(500);
+        //drawFunctionValues(100);
+        //drawArrayOfRandomValues(500);
         flip(10000);
     }
 }
+
+/******************************************************************************
+ *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
+
